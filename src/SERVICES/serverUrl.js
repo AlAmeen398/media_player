5f5b6923582e74ef97de716f19cf25af3469fd0f
AlAmeen398 <alameenn398@gmail.com>
2025-09-17 10:36:19 +0530

    'edited'

--- src/SERVICES/serverUrl.js
+++ src/SERVICES/serverUrl.js
@@ -1,2 +1,2 @@
 // export const serverUrl = 'http://localhost:5000';
-export const serverUrl='https://mediaplayer-be-l21h.onrender.com';
+export const serverUrl='https://mediaplayer-be-0uso.onrender.com';
